ff95314e14c9d12c5407f0ab44fd7d623eab5776
Micheal Gardner <twocucao@gmail.com>
2016-07-22 17:54:40 +0800

    version improved

--- danmufm/danmu.py
+++ danmufm/danmu.py
@@ -31,7 +31,7 @@ APP_DESC = """
                         ---- A Terminal Tools For DouyuTV
 
     @author Micheal Gardner (twocucao@gmail.com)
-                                    last_update 2016-06-09
+                                    last_update 2016-07-14 08:58
 """
 
 logger = logging.getLogger('danmu.fm')
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 打包的用的setup必须引入，
 """
 
-VERSION = '0.3.2'
+VERSION = '0.3.3'
 
 setup(name='danmu.fm',
       version=VERSION,
